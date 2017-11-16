ff631503248a17105a6a6b06b6ca3b719f71a35b
MWSebastienT <s.toursel@mentalworks.fr>
2017-11-16 09:10:46 +0100

    some changes

--- src/BackBundle/Controller/EntrepriseController.php
+++ src/BackBundle/Controller/EntrepriseController.php
@@ -8,10 +8,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class EntrepriseController extends Controller
 {
     /**
-     * @Route("/entreprise", name="entreprise_show")
+     * @Route("/entreprise", name="entreprise_index")
      */
     public function indexAction()
     {
         return $this->render('BackBundle:Entreprise:index.html.twig');
+    }
+
+    /**
+     * @Route("/entreprise/fiche", name="entreprise_show")
+     */
+    public function showAction()
+    {
+        return $this->render('BackBundle:Entreprise:ficheEntreprise.html.twig');
     }
 }
