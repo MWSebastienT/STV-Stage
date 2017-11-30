9724d5cd40c17dc5656c7289d3dc94f4a41bb6f1
MWSebastienT <s.toursel@mentalworks.fr>
2017-11-30 14:50:56 +0100

    commit

--- src/BackBundle/Controller/RefPedaController.php
+++ src/BackBundle/Controller/RefPedaController.php
@@ -5,6 +5,7 @@ namespace BackBundle\Controller;
 use BackBundle\Entity\Entreprise;
 use BackBundle\Entity\typeEntreprise;
 use ConnexionBundle\Entity\User;
+use ConnexionBundle\Form\RefPedaType;
 use ConnexionBundle\Form\RefProType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -12,10 +13,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 
-class RefProController extends Controller
+class RefPedaController extends Controller
 {
     /**
-     * @Route("/entreprise/show/{id}/RefPro/add", name="entreprise_add_refpro")
+     * @Route("/entreprise/show/{id}/RefPeda/add", name="entreprise_add_refpeda")
      */
     public function addRefProAction($id, Request $request)
     {
@@ -23,9 +24,9 @@ class RefProController extends Controller
         /* Config nécessaire au fonctionnement du service */
 
         $em = $this->getDoctrine()->getManager();
-        $refPro = new User();
-        $form = $this->createForm(RefProType::class, $refPro);
-        $entityName = 'RefPro';
+        $refPeda = new User();
+        $form = $this->createForm(RefPedaType::class, $refPeda);
+        $entityName = 'RefPeda';
         $listEntreprise = $em->getRepository('BackBundle:Entreprise')->findAll();
         $entreprise = $em->getRepository('BackBundle:Entreprise')->find($id);
         $action = 'edit';
@@ -33,15 +34,15 @@ class RefProController extends Controller
 
         /* l'appel du service */
 
-        $data = $this->container->get('back.method.actions')->formAction($request, $form, $refPro, $entityName,
-            $action,'ROLE_REF_PRO');// true parce que j'utilise la table User pour add
+        $data = $this->container->get('back.method.actions')->formAction($request, $form, $refPeda, $entityName,
+            $action,'ROLE_REF_PEDA');// true parce que j'utilise la table User pour add
         if ($data[0] == 'validate') // si on est dans la validation du formulaire
         {
-            $refPro->setLeEntreprise($entreprise);
-            $em->persist($refPro);
+            $refPeda->setLeEntreprise($entreprise);
+            $em->persist($refPeda);
             $em->flush();
             $session = new Session();
-            $session->getFlashBag()->add('refProOk', ''); // le message à l'utilisateur
+            $session->getFlashBag()->add('refPedaOk', ''); // le message à l'utilisateur
             return $this->redirectToRoute('entreprise_show', ['entreprises' => $listEntreprise, 'entreprise' => $entreprise, 'id' => $id]); // pas le choix d'utiliser ce redirectToRoute pour evité le chargment d'un cache de merde !!!!
         } else {// sinon on affiche le formulaire
             return new Response($data);
